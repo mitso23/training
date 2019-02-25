2ec807f39e1f67a35c82e21a26db9dfbec4bf6c2
d.symonidis <d.symonidis@samsung.com>
2019-02-25 15:33:44 +0000

    Add support for compiling for c++17

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
-export CC=g++
-export CFLAGS= -Wall -std=c++11  -g
+export CC=gcc-7  
+export CFLAGS=-Wall -std=c++11  -g
 export INCLUDES= -I${PRJ_ROOT}/include -I/usr/include/dbus-1.0/ -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include `pkg-config --cflags glib-2.0`
-export LDFLAGS= -Wl,-rpath=${PRJ_ROOT}/lib -Wl,-rpath=/usr/local/lib/ -L${PRJ_ROOT}/lib -L/usr/lib/x86_64-linux-gnu/
+export LDFLAGS= -lstdc++ -Wl,-rpath=${PRJ_ROOT}/lib -Wl,-rpath=/usr/local/lib/ -L${PRJ_ROOT}/lib -L/usr/lib/x86_64-linux-gnu/
